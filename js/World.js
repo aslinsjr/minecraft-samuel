3ba010f63299425085db3f7ab371beb283a398fe
Alexandre Lins <aslinsjr@gmail.com>
2026-01-27 13:14:44 -0300

    Update World.js

--- js/World.js
+++ js/World.js
@@ -43,7 +43,7 @@ export class World {
 
     generate() {
         const size = 30;
-        const islandRadius = 25;
+        const islandRadius = 30;
 
         this.terrainData = [];
         this.treesData = [];
